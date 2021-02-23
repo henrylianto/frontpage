2d507613713bac5e5ef6fd551e85d24f7b64fdb4
henrylianto <71800989+henrylianto@users.noreply.github.com>
2021-02-23 10:09:38 +0800

    Add files via upload

---
+++ assets/js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+var instance = M.Tabs.init(el, options);
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
